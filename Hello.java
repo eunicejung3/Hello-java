d2f544c28aed7d6ecb6a57b022880d29004225d4
eunicejung3 <69881331+eunicejung3@users.noreply.github.com>
2021-05-24 18:21:23 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/*************************************************
+*
+**************************************************
+* Eunice Jung
+* 5/24/2021
+* CMSC 255-C01
+***************************************************/
+
+public class Hello {
+	public static void main(String[] args){
+
+	System.out.println("Eunice Jung");
+	System.out.println("Lab 1");
+	System.out.println("To make sure that Java and all related applicatiions are installed.");
+	System.out.println("5/24/2021");
+	System.out.println("CMSC 255-C01");
+	System.out.println();
+
+	}
+}
